24afa5bf5406cf276f713db6ab8fa2f5031a11bc
Afiq Harith <afiqharith05@gmail.com>
2022-03-19 13:32:53 +0800

    feat: tooltip added

--- assets/static/copy_clipboard.js
+++ assets/static/copy_clipboard.js
@@ -1,5 +1,11 @@
-function copy_to_clipboard(element) {
-	var copied_text = document.getElementById(element);
+function copy_to_clipboard(element1, element2) {
+	var copied_text = document.getElementById(element1);
 	navigator.clipboard.writeText(decodeURIComponent(copied_text.innerHTML));
-	alert('Copied!');
+
+	var tooltip = document.getElementById(element2);
+	tooltip.innerHTML = 'Copied!';
+}
+function mouse_hover_out(element1) {
+	var tooltip = document.getElementById(element1);
+	tooltip.innerHTML = 'Copy to clipboard';
 }
