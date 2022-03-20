76e61c853bbc4b2b8248bb639b51704ad3a9ebf5
Afiq Harith <afiqharith05@gmail.com>
2022-03-20 08:43:42 +0800

    fix: fix minor bug on html template

--- assets/static/scripts/copy_clipboard.js
+++ assets/static/scripts/copy_clipboard.js
@@ -1,11 +1,12 @@
-function copy_to_clipboard(element1, element2) {
+let method_copy_text = (element1, element2) => {
 	var copied_text = document.getElementById(element1);
 	navigator.clipboard.writeText(decodeURIComponent(copied_text.innerHTML));
 
 	var tooltip = document.getElementById(element2);
 	tooltip.innerHTML = 'Copied!';
-}
-function mouse_hover_out(element1) {
+};
+
+let method_mouse_hover_out = element1 => {
 	var tooltip = document.getElementById(element1);
 	tooltip.innerHTML = 'Copy to clipboard';
-}
+};
